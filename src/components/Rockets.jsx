018af6b327d69c18b45df0e95de8294fa62a1f01
mosidrum <mosiokanga@gmail.com>
2023-06-19 14:21:14 +0100

    display rockets, list render

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -1,7 +1,18 @@
-const Rocket = () => (
-  <div>
-    <h1>Rockets</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const Rocket = () => {
+  const rockers = useSelector((state) => state.rockets);
+  return (
+    <ul>
+      {rockers.rocket.map((rocket) => (
+        <li key={rocket.id}>
+          <p>{rocket.rocket_name}</p>
+          <p>{rocket.description}</p>
+          <img src={rocket.flickr_images} alt={rocket.rocket_name} />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Rocket;
