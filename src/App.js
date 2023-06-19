fe6410722ac07d5055f0b12e6cc95b14273f6fee
danielsafari143 <danielsafari143@gmail.com>
2023-06-19 13:58:57 +0200

    FETCH DATA FROM MISSIONS AND ROCKETS API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchRockets } from './features/rockets/rocketsSlice';
+import { fetchMissions } from './features/missions/missionsSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRockets());
+    dispatch(fetchMissions());
+  });
+
   return (
     <div className="App">
       Hello
---
+++ src/features/rockets/rocketsSlice.js
@@ -0,0 +1,37 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  rocket: [],
+};
+
+const endpoint = 'https://api.spacexdata.com/v4/rockets';
+
+export const fetchRockets = createAsyncThunk('rocket/fetchRocket', async () => {
+  try {
+    const data = await axios.get(endpoint);
+    return data.data;
+  } catch (error) {
+    return error;
+  }
+});
+
+const rocketsSlice = createSlice({
+  name: 'rocket',
+  initialState,
+  extraReducers(builder) {
+    builder.addCase(fetchRockets.fulfilled, (state, action) => {
+      const data = action.payload;
+      data.forEach((item) => {
+        state.rocket.push({
+          id: item.id,
+          rocket_name: item.name,
+          description: item.description,
+          flickr_images: item.flickr_images,
+        });
+      });
+    });
+  },
+});
+
+export default rocketsSlice.reducer;
